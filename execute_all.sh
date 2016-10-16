069b0616c22efb848e53cc7e1a4f4154e5c7920c
B. Heinen <sfscs@web.de>
2016-10-17 00:45:06 +0100

    Use right name of the final script

--- execute_all.sh
+++ execute_all.sh
@@ -4,4 +4,4 @@ python3 /tmp/scripts/3_count_relevant_words.py && \
 # python3 /tmp/scripts/4_compute_tfidf.py
 python3 /tmp/scripts/5_compute_counts_per_review.py && \
 python3 /tmp/scripts/6_convert_to_product_and_reviewer.py && \
-python3 /tmp/scripts/7_compute_pretty_pictures.py
+python3 /tmp/scripts/7_compute_statistics.py
